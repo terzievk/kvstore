c4e9e4cd0aa1a725f633eeedfe2528b09d67497c
Konstantin Terziev <terzievk@yahoo.com>
2026-05-15 22:29:11 +0300

    it's a start

--- hashtable.h
+++ hashtable.h
@@ -3,9 +3,11 @@
 
 #include <string>
 #include <optional>
+#include <vector>
+#include <utility>
 
 class HashTable {
-
+  std::vector<std::vector<std::pair<std::string, std::string>>> v;
 
 public:
   void set(std::string key, std::string value) {}
