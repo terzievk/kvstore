0b64c9220df710e2c19bd77d4879a9e30eed6262
Konstantin Terziev <terzievk@yahoo.com>
2026-05-15 22:17:18 +0300

    baby steps

---
+++ hashtable.h
@@ -0,0 +1,16 @@
+#ifndef HASHTABLE_H_
+#define HASHTABLE_H_
+
+#include <string>
+#include <optional>
+
+class HashTable {
+
+
+public:
+  void set(std::string key, std::string value) {}
+  std::optional<std::string> get(std::string key) {}
+  void deleteValue(std::string key) {}
+};
+
+#endif  // HASHTABLE_H_
